3b3609d331921cc5c566095d634f451345309f92
badqiu <badqiu@gmail.com>
2017-08-29 10:17:15 +0800

    1

--- src/test/java/com/duowan/hummingbird/gamma/sqlparser/AnySqlTest.java
+++ src/test/java/com/duowan/hummingbird/gamma/sqlparser/AnySqlTest.java
@@ -1,24 +1,54 @@
 package com.duowan.hummingbird.gamma.sqlparser;
 
+import java.io.IOException;
 import java.io.StringReader;
-import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.parser.CCJSqlParserManager;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import com.github.rapid.common.io.freemarker.FreemarkerReader;
+
+import freemarker.template.Configuration;
 
 public class AnySqlTest {
 
 	@Test
-	public void testUse() throws JSQLParserException {
-		System.out.println(new Timestamp(1468252800000L));
-		System.out.println(System.getProperty("user.home"));
-		System.out.println(new Timestamp(1451921460000l));
-		
-		System.out.println((Integer.MAX_VALUE / 1024 / 1024) + "mb");
-		CCJSqlParserManager pm = new CCJSqlParserManager();
-		net.sf.jsqlparser.statement.Statement stmt = pm.parse(new StringReader("use abc"));
-		System.out.println("stmt:"+stmt);
+	public void testUse() throws JSQLParserException, IOException {
+		System.out.println(new ArrayList().subList(0, 6));
+		System.out.println(Arrays.asList(1,5.20,"badqiu"));
+		System.out.println(System.currentTimeMillis());
+		Map param = new HashMap();
+		param.put("day", "2017-10-10");
+		FreemarkerReader reader = new FreemarkerReader(new StringReader("<#if day?replace('-','')?number &gt; 2016 >true,${day?replace('-','')?number}</#if>"),new Configuration(),param);
+		System.out.println(IOUtils.toString(reader));
+//		System.out.println("中".length());
+//		printABCD(6);
+//		System.out.println(new Timestamp(1476806400000L));
+//		System.out.println(System.getenv("DWENV"));
+//		
+//		System.out.println("01:"+('\001' == '\1'));
+//		
+//		System.out.println(new Timestamp(1468252800000L));
+//		System.out.println(System.getProperty("user.home"));
+//		System.out.println(new Timestamp(1451921460000l));
+//		
+//		System.out.println((Integer.MAX_VALUE / 1024 / 1024) + "mb");
+//		CCJSqlParserManager pm = new CCJSqlParserManager();
+//		net.sf.jsqlparser.statement.Statement stmt = pm.parse(new StringReader("use abc"));
+//		System.out.println("stmt:"+stmt);
+	}
+
+	private void printABCD(int sum) {
+		double a = sum * 0.2;
+		double b = sum * 0.3;
+		double c = sum * 0.4;
+		double d = sum * 0.1;
+		System.out.println("A:"+a+"\nB:"+b+"\nC:"+c+"\nD:"+d+"\n\n\n");
 	}
 }
