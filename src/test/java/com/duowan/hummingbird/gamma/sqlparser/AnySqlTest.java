0ca1c9aa6faa2ae91281696925c85bc2e99999e9
badqiu <badqiu@gmail.com>
2019-11-21 14:32:44 +0800

    1

--- src/test/java/com/duowan/hummingbird/gamma/sqlparser/AnySqlTest.java
+++ src/test/java/com/duowan/hummingbird/gamma/sqlparser/AnySqlTest.java
@@ -1,38 +1,40 @@
 package com.duowan.hummingbird.gamma.sqlparser;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
 import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import net.sf.jsqlparser.JSQLParserException;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
-
-import com.github.rapid.common.io.freemarker.FreemarkerReader;
-
-import freemarker.template.Configuration;
 
 public class AnySqlTest {
 
 	@Test
 	public void testUse() throws JSQLParserException, IOException {
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		System.out.println(f.format(LocalDate.of(2017, 1, 4)));
+		
+		System.out.println(Long.MAX_VALUE);
+		/*
 		List<String> line1 = FileUtils.readLines(new File("H:\\tmp\\Mi-data\\1/fact_create.csv"));
 		List<String> line2 = FileUtils.readLines(new File("H:\\tmp\\Mi-data\\2/fact_create.csv"));
 		line2.remove(0);
 		line1.addAll(line2);
 		FileUtils.writeLines(new File("H:\\tmp\\Mi-data\\create.csv"), line1);
+		*/
 		
-		System.out.println(new Timestamp(1509246868000L));
+		System.out.println(new Timestamp(1572634920000L));
+		System.out.println(2633889679L/ 3600.0 / 1000 / 24);
+		
+		/*
 		Map param = new HashMap();
 		param.put("day", "2017-10-10");
 		FreemarkerReader reader = new FreemarkerReader(new StringReader("<#if day?replace('-','')?number &gt; 2016 >true,${day?replace('-','')?number}</#if>"),new Configuration(),param);
 		System.out.println(IOUtils.toString(reader));
+		*/
+		
 //		System.out.println("中".length());
 //		printABCD(6);
 //		System.out.println(new Timestamp(1476806400000L));
